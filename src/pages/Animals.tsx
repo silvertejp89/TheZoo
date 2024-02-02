ad551984021b8be16a7a98f72a9505c609fcea41
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-02-02 20:38:39 +0100

    getDetails

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import IAnimal from "../models/IAnimal";
 import getAnimals from "../services/animalService";
+import { useNavigate } from "react-router-dom";
 
 export const Animals = () => {
   const [animals, setAnimals] = useState<IAnimal[]>(
     JSON.parse(localStorage.getItem("animalList") || "[]")
   );
-  //i parantesen, hämta från localstorage som i webshop.
 
   useEffect(() => {
     const getData = async () => {
@@ -27,14 +27,30 @@ export const Animals = () => {
       shouldUpdate = false;
     };
   });
+
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>Jättemassa många djur</h1>
       {animals?.map((animal) => (
-        <h3>{animal.name}</h3>
+        <>
+          <h3>{animal.name}</h3>
+          <p>{animal.id}</p>
+          <button
+            onClick={() => {
+              navigate("/animals/" + animal.id);
+            }}
+          >
+            Läs mer
+          </button>
+        </>
       ))}
     </>
   );
 };
 
 export default Animals;
+
+//gör en reusable component att använda i loopen.
+//komponenten ska innehålla en knapp som skickar till detail (id via url)
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,5 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import IAnimal from "../models/IAnimal";
+
+//TODO: få in listan från localstorage, loopa och visa description och state. knapp för att ändra state.
+
 export const Details = () => {
-  return <h1>Details</h1>;
+  const { animalId } = useParams();
+  const [animal, setAnimal] = useState<IAnimal | null>(null);
+
+  //useEffect, hämta listan från localstorage
+  useEffect(() => {
+    const storedAnimals = JSON.parse(
+      localStorage.getItem("animalList") || "[]"
+    );
+    //The find() method returns the value of the first element that passes a test.
+    //Hitta djuret baserat på id.
+    const selectedAnimal = storedAnimals.find(
+      (a: IAnimal) => a.id.toString() === animalId
+    );
+
+    setAnimal(selectedAnimal);
+  }, [animalId]);
+
+  if (!animal) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h1>Details: {animal.name}</h1>;
+    </>
+  );
 };
 
 export default Details;
